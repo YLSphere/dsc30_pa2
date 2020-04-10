1a7af1916ac2306002aa169aaa7605e75a96dd35
YLSphere <Myron2001922>
2020-04-09 18:09:48 -0700

    PA2

--- src/IntStack.java
+++ src/IntStack.java
@@ -1,6 +1,6 @@
 /*
-    Name: TODO
-    PID:  TODO
+    Name: Yin Lam Lai
+    PID:  A15779757
  */
 
 import java.util.EmptyStackException;
@@ -12,71 +12,172 @@ import java.util.EmptyStackException;
  */
 public class IntStack {
 
-    /* static constants, feel free to add more if you need */
-    private static final int    MIN_INIT_CAPACITY = 3;
-    private static final int    RESIZE_FACTOR     = 2;
-    private static final double DEF_LOAD_FACTOR   = 0.75;
-    private static final double MIN_LOAD_FACTOR   = 0.6;
+    private static final int MIN_INIT_CAPACITY = 3;
+    private static final int RESIZE_FACTOR = 2;
+    private static final double DEF_LOAD_FACTOR = 0.75;
+    private static final double MIN_LOAD_FACTOR = 0.6;
     private static final double DEF_SHRINK_FACTOR = 0.2;
     private static final double MAX_SHRINK_FACTOR = 0.4;
 
-    /* instance variables, feel free to add more if you need */
     private int[] data;
     private int nElems;
     private double loadFactor;
     private double shrinkFactor;
+    private int initialCap;
 
     public IntStack(int capacity, double loadF, double shrinkF) {
-        /* TODO */
+        if (capacity < MIN_INIT_CAPACITY) {
+            throw new IllegalArgumentException("Capacity out of range");
+        } else if (MIN_LOAD_FACTOR > loadF) {
+            throw new IllegalArgumentException("Load Factor out of range");
+        } else if (loadF > 1) {
+            throw new IllegalArgumentException("Load Factor out of range");
+        } else if (0 > shrinkF) {
+            throw new IllegalArgumentException("Shrink Factor out of range");
+        } else if (shrinkF > MAX_SHRINK_FACTOR) {
+            throw new IllegalArgumentException("Shrink Factor out of range");
+        } else {
+            this.initialCap = capacity;
+            this.nElems = 0;
+            this.data = new int[initialCap];
+            this.loadFactor = loadF;
+            this.shrinkFactor = shrinkF;
+        }
     }
 
     public IntStack(int capacity, double loadF) {
-        /* TODO */
+        if (capacity < MIN_INIT_CAPACITY) {
+            throw new IllegalArgumentException("Capacity out of range");
+        } else if (MIN_LOAD_FACTOR > loadF) {
+            throw new IllegalArgumentException("Load Factor out of range");
+        } else if (loadF > 1) {
+            throw new IllegalArgumentException("Load Factor out of range");
+        } else {
+            this.initialCap = capacity;
+            this.data = new int[initialCap];
+            this.nElems = 0;
+            this.loadFactor = loadF;
+            this.shrinkFactor = DEF_SHRINK_FACTOR ;
+        }
     }
 
     public IntStack(int capacity) {
-        /* TODO */
+        if (capacity < MIN_INIT_CAPACITY) {
+            throw new IllegalArgumentException("Capacity out of range");
+        } else {
+            this.initialCap = capacity;
+            this.data = new int[this.initialCap];
+            this.nElems = 0;
+            this.loadFactor = DEF_LOAD_FACTOR;
+            this.shrinkFactor = DEF_SHRINK_FACTOR ;
+        }
     }
 
     public boolean isEmpty() {
-        /* TODO */
+        if (nElems == 0){
+            return true;
+        }
         return false;
     }
 
     public void clear() {
-        /* TODO */
+        data = new int[initialCap];
+        this.nElems = 0;
     }
 
     public int size() {
-        /* TODO */
-        return 0;
+        return nElems;
     }
 
     public int capacity() {
-        /* TODO */
-        return 0;
+        return data.length;
     }
 
     public int peek() {
-        /* TODO */
-        return 0;
+        if (nElems == 0) {
+            throw new EmptyStackException();
+        }
+        return data[nElems - 1];
     }
 
     public void push(int element) {
-        /* TODO */
+        double sizeD = this.size();
+        double capacityD = this.capacity();
+        if (sizeD / capacityD >= loadFactor) {
+            int[] temp = new int[RESIZE_FACTOR * capacity()];
+
+            for(int n = 0; n < nElems; n++) {
+                temp[n] = data[n];
+            }
+            data = temp;
+        }
+        data[nElems] = element;
+        nElems++;
     }
 
     public int pop() {
-        /* TODO */
-        return 0;
+        if (nElems == 0) {
+            throw new EmptyStackException();
+        } else {
+            int tempop = data[nElems - 1];
+            data[nElems - 1] = 0;
+            nElems--;
+            double sizeD = this.size();
+            double capacityD = this.capacity();
+            if (sizeD / capacityD <= shrinkFactor) {
+
+
+                if (this.capacity() / RESIZE_FACTOR < initialCap) {
+                    int[] temp = new int[capacity()];
+                    for(int n = 0; n < nElems; n++) {
+                        temp[n] = data[n];
+                    }
+                    data = temp;
+                } else {
+                    int[] temp = new int[capacity() / RESIZE_FACTOR];
+
+                    for(int n = 0; n < nElems; n++) {
+                        temp[n] = data[n];
+                    }
+                    data = temp;
+                }
+            }
+
+            return tempop;
+        }
     }
 
     public void multiPush(int[] elements) {
-        /* TODO */
+        if (elements == null) {
+            throw new IllegalArgumentException("Elements is null!");
+        }
+        for(int n : elements) {
+            this.push(n);
+        }
+
+
     }
 
     public int[] multiPop(int amount) {
-        /* TODO */
-        return null;
+        if (amount < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            int temp = nElems;
+
+            int[] results;
+            if (temp >= amount) {
+                results = new int[amount];
+                for(int n = 0; n < amount; n++) {
+                    results[n] = this.pop();
+                }
+                return results;
+            } else {
+                results = new int[temp];
+                for(int n = 0; n < temp; n++) {
+                    results[n] = this.pop();
+                }
+                return results;
+            }
+        }
     }
 }
---
+++ src/IntStackTest.java
@@ -0,0 +1,165 @@
+import static org.junit.Assert.*;
+import java.util.EmptyStackException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntStackTest {
+    IntStack test1;
+    IntStack test2;
+    IntStack test3;
+
+    public IntStackTest() {
+    }
+
+    @Before
+    public void setup() throws Exception {
+        test1 = new IntStack(4, 0.7, 0.25);
+        test2 = new IntStack(4, 0.7);
+        test3 = new IntStack(4);
+    }
+
+    @Test
+    public void isEmpty() {
+        assertTrue(test1.isEmpty());
+        test2.push(1);
+        assertFalse(test2.isEmpty());
+        assertTrue(test3.isEmpty());
+    }
+
+    @Test
+    public void clear() {
+        int[] pushed = new int[]{1, 2, 3, 4, 5};
+        test1.multiPush(pushed);
+        test1.clear();
+        assertTrue(test1.isEmpty());
+        test2.push(1);
+        test2.clear();
+        assertTrue(test2.isEmpty());
+        test3.clear();
+        assertTrue(test2.isEmpty());
+    }
+
+    @Test
+    public void size() {
+        test1.push(1);
+        test1.push(1);
+        test1.push(1);
+        assertEquals(3L, (long)test1.size());
+        assertEquals(0L, (long)test2.size());
+        test3.push(1);
+        test3.push(1);
+        test3.push(1);
+        test3.push(1);
+        test3.push(1);
+        test3.push(1);
+        test3.push(1);
+        test3.push(1);
+        test3.push(1);
+        assertEquals(9L, (long)test3.size());
+    }
+
+    @Test
+    public void capacity() {
+        assertEquals(4L, (long)test1.capacity());
+        test2.push(1);
+        test2.push(1);
+        test2.push(1);
+        test2.push(1);
+        assertEquals(8L, (long)test2.capacity());
+        test3.push(1);
+        assertEquals(4L, (long)test3.capacity());
+    }
+
+    @Test
+    public void peek() {
+        try {
+            assertEquals(4, (long)test1.peek());
+        } catch (EmptyStackException var2) {
+            System.out.println("Stack is empty!");
+        }
+
+        test2.push(1);
+        test2.push(2);
+        test2.push(3);
+        test2.push(4);
+        assertEquals(4, test2.peek());
+        test3.push(1);
+        assertEquals(1, test3.peek());
+    }
+
+    @Test
+    public void push() {
+        test1.push(1);
+        assertEquals(1, test1.peek());
+        test2.push(1);
+        test2.push(2);
+        test2.push(3);
+        test2.push(4);
+        assertEquals(4, test2.peek());
+        test3.push(-3);
+        assertEquals(-3, test3.peek());
+    }
+
+    @Test
+    public void pop() {
+        try {
+            test1.pop();
+        } catch (EmptyStackException var2) {
+            System.out.println("Stack is empty!");
+        }
+
+        test2.push(1);
+        test2.pop();
+        assertTrue(test2.isEmpty());
+        int[] pushed = new int[]{1, 2, 3, 4, 5};
+        test3.multiPush(pushed);
+        test3.pop();
+        test3.pop();
+        test3.pop();
+        test3.pop();
+        test3.pop();
+        assertTrue(test3.isEmpty());
+    }
+
+    @Test
+    public void multiPush() {
+        int[] pushed1 = new int[]{1, 2, 3, 4, 5};
+        int[] pushed2 = null;
+        test1.multiPush(pushed1);
+        assertEquals(5, test1.peek());
+
+        try {
+            test2.multiPush(pushed2);
+        } catch (IllegalArgumentException var5) {
+            System.out.println(var5.getMessage());
+        }
+
+        test3.multiPush(pushed1);
+        test3.multiPush(pushed1);
+        assertEquals(5, test3.peek());
+    }
+
+    @Test
+    public void multiPop() {
+        try {
+            test1.multiPop(-1);
+        } catch (IllegalArgumentException var3) {
+            System.out.println("Number is negative!");
+        }
+
+        test2.push(1);
+        test2.push(2);
+        test2.push(3);
+        test2.push(4);
+        test2.push(5);
+        int[] expected = new int[]{5, 4, 3, 2, 1};
+        assertArrayEquals(expected, test2.multiPop(5));
+
+        test3.push(1);
+        test3.push(2);
+        test3.push(3);
+        int[] expected1 = new int[]{3, 2, 1};
+        assertArrayEquals(expected1, test3.multiPop(6));
+    }
+}
